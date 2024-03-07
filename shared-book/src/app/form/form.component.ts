46a0a8d6af962023045a212d607f6a9d4e826589
BarrasLise <barraslise@gmail.com>
2024-03-07 09:19:23 +0100

    first commit for today 07/03

--- shared-book/src/app/form/form.component.ts
+++ shared-book/src/app/form/form.component.ts
@@ -53,45 +53,6 @@ export class FormComponent {
     }
   }
 
-  // posts: Post[];
-  // newPostTitle: string;
-  // newPostDescription: string;
-  // newPostLocation: string;
-  // newPostImageUrl: string;
-
-  // postAdded: boolean = false;
-
-  // constructor(private postsService: PostsService) { }
-
-  // ngOnInit(): void {
-  //   this.posts = this.postsService.getPosts();
-  // }
-
-  // addPost(): void {
-
-    
-  //   if (this.newPostTitle && this.newPostDescription && this.newPostImageUrl) {
-  //     const newPost = new Post(
-  //       this.newPostTitle,
-  //       this.newPostDescription,
-  //       new Date(),
-  //       this.newPostImageUrl,
-  //       0, // Initial likes count
-  //       this.newPostLocation,
-  //     );
-  //     this.postsService.addPost(newPost);
-  //     this.postAdded = true;
-  //     // Réinitialiser le formulaire
-  //     this.resetForm();
-  //   }
-  // }
-
-  // resetForm() {
-  //   this.newPostTitle = '';
-  //   this.newPostDescription = '';
-  //   this.newPostLocation = '';
-  //   this.newPostImageUrl = '';
-    
-  // }
+  
 
 }
